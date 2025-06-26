feb65dc8838019f060b1c11cdccf5f690abeac66
swawiye <sovereign.wawiye@gmail.com>
2025-06-26 08:35:19 +0300

    addition of views

--- counterapp/models.py
+++ counterapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class FoodItems(models.Model): # instance for a table
     name = models.CharField(max_length=255) 
     calories = models.PositiveIntegerField()
-    date_added = models.DateField()
+    date_added = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.name
