5dbf8657a69cde6f41f53bf2492e20079d5a6776
swawiye <sovereign.wawiye@gmail.com>
2025-06-26 07:51:26 +0300

    setting up the db and configuration

--- counterapp/models.py
+++ counterapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class FoodItems(models.Model): # instance for a table
+    name = models.CharField(max_length=255) 
+    calories = models.PositiveIntegerField()
+    date_added = models.DateField()
+    
+    def __str__(self):
+        return self.name
