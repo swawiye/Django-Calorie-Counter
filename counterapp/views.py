6730ee4ce8e917df92e22a2ca30195683bef3675
swawiye <sovereign.wawiye@gmail.com>
2025-06-26 21:59:00 +0300

    pages and styling

--- counterapp/views.py
+++ counterapp/views.py
@@ -6,23 +6,3 @@ from datetime import date
 def index(request) :
     return render(request, 'index.html')
 
-def food_list(request):
-    today = date.today()
-    food_items = FoodItems.objects.filter(date_added=today)
-    total_calories = sum(item.calories for item in food_items)
-    return render(request, 'index.html', {'food_items': food_items, 'total_calories': total_calories})
-
-def add_food(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        calories = request.POST['calories']
-        FoodItems.objects.create(name=name, calories=calories)
-    return render(request, 'index.html')
-
-def delete_food(request, food_id):
-    FoodItems.objects.get(id=food_id).delete()
-    return render(request, 'index.html')
-
-def reset_day(request):
-    FoodItems.objects.filter(date_added=date.today()).delete()
-    return render(request, 'index.html')
